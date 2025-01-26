941c6005a6dee661687dd166c12c1b33b73d11b5
ranger-3 <ianik123450909w@gmail.com>
2025-01-26 18:38:52 +0200

    Delete lab_2 directory

--- lab_2/src/index.js
+++
@@ -1,8 +0,0 @@
-import { getRandomActivity } from "./activity.js";
-
-const activity = document.getElementById('activity');
-
-document.getElementById('get-activity-btn').addEventListener('click', async function() {
-    const result = await getRandomActivity();
-    activity.innerHTML = result;
-});
